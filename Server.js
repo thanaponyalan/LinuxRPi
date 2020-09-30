24d6a386275beaca9943e89a8b5dd91c295bb454
thanaponyalan <thanaponyalan@gmail.com>
2020-09-30 15:21:52 +0700

    test mongoose

--- Server.js
+++ Server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-var mongoose=require('mongoose');
+var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/s60030037');
-var db=mongoose.connection;
+var db = mongoose.connection;
 
 // app.get('/ping', function(req, res) {
 //     res.send({ping:'hello this is server and I am alive!'});
@@ -12,6 +12,14 @@ var db=mongoose.connection;
 // app.get('/ping/:id', function(req, res) {
 //     res.send({ping:'hello this is server and I am got '+req.params.id});
 // });
+
+db.on('error', function callback() {
+    console.log("Connection error");
+});
+
+db.once('open', function callback() {
+    console.log("Mongo working!");
+});
 
 app.listen(3009);
 console.log('Listening on port 3009...');
