7aaa646db7bd07f1da35a2537b01d9f87d69ff14
thanaponyalan <thanaponyalan@gmail.com>
2020-09-30 15:12:04 +0700

    mongoose

--- Server.js
+++ Server.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
+var mongoose=require('mongoose');
 
-app.get('/ping', function(req, res) {
-    res.send({ping:'hello this is server and I am alive!'});
-});
+mongoose.connect('mongodb://localhost/s60030037');
+var db=mongoose.connection;
 
-app.get('/ping/:id', function(req, res) {
-    res.send({ping:'hello this is server and I am got '+req.params.id});
-});
+// app.get('/ping', function(req, res) {
+//     res.send({ping:'hello this is server and I am alive!'});
+// });
+
+// app.get('/ping/:id', function(req, res) {
+//     res.send({ping:'hello this is server and I am got '+req.params.id});
+// });
 
 app.listen(3009);
 console.log('Listening on port 3009...');
